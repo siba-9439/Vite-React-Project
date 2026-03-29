5ae70cdfe7db544c6be58cc533479e2f8af6d71e
Siba Prasad <147345550+siba-9439@users.noreply.github.com>
2026-03-29 16:28:35 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';  // 👈 import HashRouter
-import App from './App.tsx';
-import './index.css';
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
